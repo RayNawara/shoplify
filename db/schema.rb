741475e3242d25c2107e8115965bd6f029f04900
RayNawara <rjnawara@gmail.com>
2021-04-20 15:46:06 -0400

    add sales_count to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_165859) do
+ActiveRecord::Schema.define(version: 2021_04_20_193621) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_165859) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "sales_count", default: 0, null: false
   end
 
 end
