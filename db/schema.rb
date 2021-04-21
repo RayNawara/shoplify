080d5e0a14fc401ca957ae1b838c071c9f46b1af
RayNawara <rjnawara@gmail.com>
2021-04-21 10:26:34 -0400

    Change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_20_193621) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
