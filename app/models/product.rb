aa03296ef11cfe068718dd5425aa20b10bb6602e
RayNawara <rjnawara@gmail.com>
2021-04-22 16:18:50 -0400

    Add multiple items to shopping cart and fix buy now for multiple items

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,16 @@ class Product < ApplicationRecord
 
   validates :name, :price, presence: true
 
-  # def to_s
-  #   name
-  # end
+  def to_s
+    name
+  end
+
+  def to_builder
+    Jbuilder.new do |product|
+      product.price stripe_price_id
+      product.quantity 1
+    end
+  end
 
   after_create do
     product = Stripe::Product.create(name: name)
